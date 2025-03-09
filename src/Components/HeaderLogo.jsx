0bb5d77b2496527e904783016216a0f27c7b83c2
chaitanyaGosavi <97080993+chaitanyaGosavi@users.noreply.github.com>
2025-03-09 23:47:17 +0530

    CSS Layout changes

--- src/Components/HeaderLogo.jsx
+++ src/Components/HeaderLogo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeaderLogo = () => {
   return (
-    <div className="w-full h-[10vh] text-2xl md:text-6xl text-center flex justify-center items-center text-[#78a070] HeaderLogo">
+    <div className="w-full h-[10vh] text-2xl md:text-5xl text-center flex justify-center items-center text-[#78a070] HeaderLogo">
       ✈️ Your Trip Essentials Hub 🧳
     </div>
   );
